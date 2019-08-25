a4db4ab6e6c6e740b941419b530e2d96f2ac28fd
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-08-25 16:50:00 +0530

    INITIAL: Spring boot skeleton

---
+++ java8-streams/src/main/java/poc/java/StreamsApp.java
@@ -0,0 +1,33 @@
+package poc.java;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import poc.java.service.StreamService;
+
+/**
+ * @author ashishb888
+ */
+
+@SpringBootApplication
+public class StreamsApp implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private StreamService streamService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("run service");
+
+		streamService.main();
+	}
+
+}
---
+++ java8-streams/src/main/java/poc/java/domain/Employee.java
@@ -0,0 +1,8 @@
+package poc.java.domain;
+
+/**
+ * @author ashishb888
+ */
+public class Employee {
+
+}
