779817921903874a8189b8b1fdba80575ff6d618
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-08-25 19:36:49 +0530

    INITIAL: Spring boot skeleton & with dummy test classes

---
+++ java-concurrent/src/main/java/poc/java/service/ConcurrentService.java
@@ -0,0 +1,19 @@
+package poc.java.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ashishb888
+ */
+
+@Service
+public class ConcurrentService {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public void main() {
+		logger.info("main service");
+
+	}
+}
