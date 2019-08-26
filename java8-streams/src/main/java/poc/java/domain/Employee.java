46f147f36646336e63b23b04e47f2e2aee1557f6
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-08-26 14:50:03 +0530

    Lombok changes

--- java8-streams/src/main/java/poc/java/domain/Employee.java
+++ java8-streams/src/main/java/poc/java/domain/Employee.java
@@ -1,5 +1,6 @@
 package poc.java.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,75 +11,16 @@ import lombok.ToString;
  * @author ashishb888
  */
 
-//@Setter
-//@Getter
-//@NoArgsConstructor
-//@ToString
-//@EqualsAndHashCode
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Employee {
 	private String name;
 	private int age;
 	private double salary;
 	private String department;
 	private Gender gender;
-
-	public Employee() {
-		super();
-	}
-
-	public Employee(String name, int age, double salary, String department, Gender gender) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.salary = salary;
-		this.department = department;
-		this.gender = gender;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-	public Gender getGender() {
-		return gender;
-	}
-
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", age=" + age + ", salary=" + salary + ", department=" + department
-				+ ", gender=" + gender + "]";
-	}
-
 }
