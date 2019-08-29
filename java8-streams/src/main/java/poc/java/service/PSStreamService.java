ab55dc0e68ca66ff55992fc42e4d22d2268c6412
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-08-29 18:15:24 +0530

    Added aggregate functions for parallel stream

--- java8-streams/src/main/java/poc/java/service/PSStreamService.java
+++ java8-streams/src/main/java/poc/java/service/PSStreamService.java
@@ -56,7 +56,25 @@ public class PSStreamService {
 		List<Integer> numbers = IntStream.iterate(1, i -> i + 1).limit(10).boxed().collect(Collectors.toList());
 
 		// numbers.stream().forEach(System.out::println);
-		numbers.stream().parallel().map(e -> transform(e)).forEach(e -> log.info("e: " + e));
+		numbers.stream().parallel().map(e -> transform(e));// .forEach(e -> log.info("e: " + e));
+
+		int min = numbers.stream().parallel().mapToInt(i -> i).min().getAsInt();
+		log.info("min: " + min);
+
+		int max = numbers.stream().parallel().mapToInt(i -> i).max().getAsInt();
+		log.info("max: " + max);
+
+		int sum = numbers.stream().parallel().mapToInt(i -> i).sum();
+		log.info("sum: " + sum);
+
+		int first = numbers.stream().parallel().findFirst().get();
+		log.info("first: " + first);
+
+		int last = numbers.stream().parallel().reduce((f, s) -> s).get();
+		log.info("last: " + last);
+
+		last = numbers.stream().parallel().skip(numbers.stream().count() - 1).findFirst().get();
+		log.info("last: " + last);
 	}
 
 	public void main() {
