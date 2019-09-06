2f9009535bdd1e950b986c05b1f0618297ef4e50
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-09-06 15:33:08 +0530

    Added ObservableService: from method

--- java-rx/src/main/java/poc/java/service/ObservableService.java
+++ java-rx/src/main/java/poc/java/service/ObservableService.java
@@ -1,5 +1,6 @@
 package poc.java.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import io.reactivex.Observable;
 import lombok.extern.slf4j.Slf4j;
 import poc.java.domain.Employee;
+import poc.java.domain.Gender;
 import poc.java.repos.EmployeeRepository;
 
 @Service
@@ -29,9 +31,42 @@ public class ObservableService {
 		});
 	}
 
+	private void from() {
+		log.info("from service");
+
+		List<Employee> employees = er.getAll(5);
+
+		Observable<Employee> observable = Observable.fromArray(employees.toArray(new Employee[employees.size()]));
+
+		observable.subscribe(data -> {
+			log.info("data: " + data);
+		}, error -> {
+			log.error("error", error);
+		}, () -> {
+			log.info("Completed");
+		});
+
+		observable.filter(e -> e.getGender() == Gender.MALE).subscribe(data -> {
+			log.info("data: " + data);
+		}, error -> {
+			log.error("error", error);
+		}, () -> {
+			log.info("Completed");
+		});
+
+		observable.filter(e -> e.getGender() == Gender.FEMALE).subscribe(data -> {
+			log.info("data: " + data);
+		}, error -> {
+			log.error("error", error);
+		}, () -> {
+			log.info("Completed");
+		});
+	}
+
 	public void main() {
 		log.info("main service");
 
-		just();
+		// just();
+		from();
 	}
 }
