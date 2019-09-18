2f66fdb3f0536eb3f0eadefb8778dce55fa5bc0f
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-09-18 16:09:32 +0530

    Added

--- jackson/src/main/java/poc/java/service/JsonNodeService.java
+++ jackson/src/main/java/poc/java/service/JsonNodeService.java
@@ -1,6 +1,15 @@
 package poc.java.service;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder;
 
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
@@ -10,13 +19,29 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings("unused")
 public class JsonNodeService {
 
-	private void csvToJson() {
-		log.info("csvToJson serive");
+	private void jsonToCsv() {
+		log.info("jsonToCsv serive");
+
+		try {
+			JsonNode jsonTree = new ObjectMapper().readTree(new File("src/main/resources/test.json"));
+
+			Builder csvBuilder = CsvSchema.builder();
+			JsonNode first = jsonTree.elements().next();
+			first.fieldNames().forEachRemaining(csvBuilder::addColumn);
+			CsvSchema csvSchema = csvBuilder.build().withHeader();
+
+			CsvMapper csvMapper = new CsvMapper();
+			csvMapper.writerFor(JsonNode.class).with(csvSchema).writeValue(new File("src/main/resources/test.csv"),
+					jsonTree);
+
+		} catch (IOException e) {
+			log.error("", e);
+		}
 	}
 
 	public void main() {
 		log.info("main service");
 
-		csvToJson();
+		jsonToCsv();
 	}
 }
