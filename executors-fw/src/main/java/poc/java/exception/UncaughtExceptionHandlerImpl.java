c33a880f9e7734ea27835841397170e018b16c30
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-12-02 11:32:22 +0530

    executors-fw: Removed unnecessary logs

--- executors-fw/src/main/java/poc/java/exception/UncaughtExceptionHandlerImpl.java
+++ executors-fw/src/main/java/poc/java/exception/UncaughtExceptionHandlerImpl.java
@@ -10,7 +10,6 @@ public class UncaughtExceptionHandlerImpl implements UncaughtExceptionHandler {
 	@Override
 	public void uncaughtException(Thread t, Throwable e) {
 		log.error(e.getMessage(), e);
-		log.debug("Thread state: " + t.getState());
 	}
 
 }
