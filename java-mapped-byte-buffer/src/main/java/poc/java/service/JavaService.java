8c73b9d9a7be6aae317527a3baca5c3d0fdedc72
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-01-29 11:52:47 +0530

    java-mapped-byte-buffer: Initial commit

---
+++ java-mapped-byte-buffer/src/main/java/poc/java/service/JavaService.java
@@ -0,0 +1,14 @@
+package poc.java.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class JavaService {
+
+	public void main() {
+		log.debug("main service");
+	}
+}
