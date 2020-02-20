a6addefdea35757fbcc6312f5fc5d38fbaf5cb7c
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-02-20 16:53:26 +0530

    java8-streams: Added ExtendWith annotation

--- java8-streams/src/test/java/poc/java/StreamsAppTest.java
+++ java8-streams/src/test/java/poc/java/StreamsAppTest.java
@@ -3,11 +3,14 @@ package poc.java;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import poc.java.service.ExampleService;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class StreamsAppTest {
 
