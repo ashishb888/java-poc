e0d29a4f2482a21fe5e4c4b2b01931f67d8d9e67
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-02-24 16:39:12 +0530

    java-byte-buddy: Initial commit

---
+++ java-byte-buddy/src/main/java/poc/java/domain/ArithmeticImpl.java
@@ -0,0 +1,10 @@
+package poc.java.domain;
+
+public class ArithmeticImpl implements Arithmetic {
+
+	@Override
+	public int perform(int a, int b) {
+		return 0;
+	}
+
+}
---
+++ java-byte-buddy/src/main/java/poc/java/domain/Fruit.java
@@ -0,0 +1,21 @@
+package poc.java.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Fruit {
+	private String name;
+	private double price;
+	private int quantity;
+
+}
