710293f5aad9d7c9fb1010ad62ee7a8548781003
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-02-25 12:34:21 +0530

    java-agents: Initial commit

---
+++ java-agents/src/main/java/poc/java/service/JavaService.java
@@ -0,0 +1,21 @@
+package poc.java.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ashishb888
+ */
+
+@Slf4j
+@Service
+public class JavaService {
+
+	@Autowired
+	public void main() {
+		log.debug("main service");
+
+	}
+}
