40debee65b4026599a68333e439cd46d2c45a340
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-03-17 14:33:32 +0530

    edabit-challenge: Initial commit

---
+++ edabit-challenge/src/main/java/poc/java/JavaApp.java
@@ -0,0 +1,33 @@
+package poc.java;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+import poc.java.service.JavaService;
+
+/**
+ * @author ashishb888
+ */
+
+@Slf4j
+@SpringBootApplication
+public class JavaApp implements CommandLineRunner {
+
+	@Autowired
+	private JavaService js;
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.debug("run service");
+
+		js.main();
+	}
+
+}
---
+++ edabit-challenge/src/main/java/poc/java/service/JavaService.java
@@ -0,0 +1,18 @@
+package poc.java.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ashishb888
+ */
+
+@Slf4j
+@Service
+public class JavaService {
+
+	public void main() {
+		log.debug("main service");
+	}
+}
