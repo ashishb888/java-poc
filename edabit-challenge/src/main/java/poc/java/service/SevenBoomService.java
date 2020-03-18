69ab4871a5d73748f1ab15b236f74b736b6cc474
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-03-18 16:01:04 +0530

    edabit-challenge: Seven Boom program: Alternative approach

--- edabit-challenge/src/main/java/poc/java/service/SevenBoomService.java
+++ edabit-challenge/src/main/java/poc/java/service/SevenBoomService.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Service;
 public class SevenBoomService {
 
 	public String sevenBoom(int[] arr) {
+
+		// return Arrays.toString(arr).contains("7") ? "Boom!" : "there is no 7 in the
+		// array";
+
 		for (int i = 0; i < arr.length; i++) {
 			if (String.valueOf(arr[i]).contains("7"))
 				return "Boom!";
