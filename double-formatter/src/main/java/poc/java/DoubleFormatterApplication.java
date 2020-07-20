68d58ab39f811bc9a5674564c3a326b9f9d5d98e
ashishb888 <ashish.bhosle008@gmail.com>
2020-07-20 13:04:57 +0530

    double-formatter: Initial commit

---
+++ double-formatter/src/main/java/poc/java/DoubleFormatterApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleFormatterApplication.class, args);
+	}
+
+}
